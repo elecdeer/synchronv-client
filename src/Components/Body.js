570f5317ed4183943fa5f47c35cfe2ee822b35a8
elecdeer <deersub@gmail.com>
2020-05-09 14:51:10 +0900

    メイン部のレイアウト

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Body = () => {
   return (
-    <div>
+    <Container>
+      <Row>
+        <Col md={8}>
 
-    </div>
+        </Col>
+        <Col md={4}>
+
+        </Col>
+      </Row>
+
+      <Row>
+        <Col md={12}>
+
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
