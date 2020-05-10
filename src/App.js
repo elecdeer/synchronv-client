70fd9bec4432ca67b6ed99296d5244a0ad7f428c
elecdeer <deersub@gmail.com>
2020-05-10 16:54:30 +0900

    ファイル分割とSocketの接続

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, {createContext, useEffect, useState} from 'react';
 import './App.css';
 import {Navbar, NavbarBrand} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Body from "./Components/Body";
+import io from "socket.io-client";
+import Header from "./Components/Header";
 
-function App(){
+export const SocketContext = createContext(null);
+
+const App = () => {
+
+  const [socketIO, setSocketIO] = useState(null);
+
+  useEffect(() => {
+    const url = `${window.location.hostname}:5001`;
+    const socketIO = io(url);
+
+    socketIO.on("connect", () => {
+      console.log("Connected!", socketIO.id);
+      // socketIO.emit("join");
+    })
+
+    //接続時の処理もここに
+
+    setSocketIO(socketIO);
+  }, []);
+
+
   return (
-    <>
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand>
-          <h3>Synchronv</h3>
-        </Navbar.Brand>
-      </Navbar>
+    <SocketContext.Provider value={socketIO}>
+      <Header/>
       <Body/>
-    </>
+    </SocketContext.Provider>
   );
+
 }
 
 export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from "react-bootstrap/Navbar";
+
+const Header = () => {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand>
+        <h3>Synchronv</h3>
+      </Navbar.Brand>
+    </Navbar>
+  );
+};
+
+export default Header;
+
